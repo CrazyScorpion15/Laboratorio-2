35073e62ca673edec48c37879a5f1d0908edd9e3
EDRYAN ANDRE LOPEZ LARIOS <ealopezla@correo.url.edu.gt>
2022-02-16 18:38:28 -0600

    Controladores y Modelo Team

---
+++ Laboratorio2/Helpers/Data.cs
@@ -0,0 +1,36 @@
+﻿using Laboratorio2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio2.Helpers
+{
+    public class Data
+    {
+        private static Data _instance = null;
+
+        public static Data Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Data();
+                }
+                return _instance;
+            }
+        }
+
+        public List<TeamModel> TeamList = new List<TeamModel>
+        {
+            new TeamModel
+            {
+                Name = "Saber",
+                Coach = "Buenas",
+                Legue = "Como",
+                CreationDate = "2/3/2002"
+            }
+        };
+    }
+}
